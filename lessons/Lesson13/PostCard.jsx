9cd4d7edb244ef160000bb2e12eedb46b646dc5a
unknown <hienng16091988@gmail.com>
2026-05-25 08:55:42 +0700

    read blog posts

---
+++ lessons/Lesson13/PostCard.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function PostCard({ post }){
+
+    return (
+        <div className="border border-slate-400 border-dashed p-4 rounded-md h-full">
+                <p className="text-slate-400 text-xs">created at</p>
+                <Link href="" className="block text-xl font-semibold mb-4">{ post.title }</Link>
+
+                <p className="text-sm"> {post.content} </p>
+              </div>
+    )
+}
